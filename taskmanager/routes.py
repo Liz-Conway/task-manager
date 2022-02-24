8b70952f3364996836524c5433e035ed8c26103e
Fintan <fintan@waycontech.com>
2022-02-24 19:41:47 +0000

    FEAT: Import Task & Category ORM classes

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -6,6 +6,10 @@ Created on 21 Feb 2022
 from flask import render_template
 from taskmanager import app, db
 
+# Need to manually create the database
+# But the tables will be automatically generated
+# Need to import model classes in order to generate the db schema in Postgres
+from taskmanager.models import Task, Category
 
 # Simple route using root directory
 # Target a function called 'home()' which returns the rendered template of "base.html"
