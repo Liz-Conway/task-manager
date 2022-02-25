26bf0d7e2e91d19b10875ac8b7efa7a8cbb28eaf
Fintan <fintan@waycontech.com>
2022-02-25 05:02:24 +0000

    FEAT: Add new Categories to the DB
    This is the 'C' in CRUD.
    Page with a form that POSTs form data to the Controller method.
    Controller method uses session.add() and session.commit() to add form data to the DB

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -16,3 +16,27 @@ from taskmanager.models import Task, Category
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html", categories = categoriesVar)
+
+'''Need to add GET and POST methods since we will be
+submitting a form to the DB
+GET = When the user clicks a link to the 'Add Category' page
+POST when the user submits the form'''
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        # Create a Category ORM object, setting the name to the name added in the form
+        category = Category(category_name = request.form.get("category_name"))
+        # Add the form data to the database
+        db.session.add(category)
+        db.session.commit()
+        '''Run the categories() method to show the 'Categories' page
+        after submitting the new category'''
+        return redirect(url_for("categories"))
+    
+    return render_template("add_category.html")
+
+
