a011b204c6ae3949ed6fa86945cd86c3e522e58f
Fintan <fintan@waycontech.com>
2022-02-25 13:06:18 +0000

    DEPLOY:  Update init file (__init__.py)
    Set the initialiser to use a local DB if it is on our PC.
    Set it to use Heroku's DB if the application is running on Heroku.

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -28,7 +28,12 @@ app = Flask(__name__)
 
 # Specify app configuration variables
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+# If we are working on our local PC
+if os.environ.get("DEVELOPMENT") == True:
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+# Otherwise we are running the application in Heroku
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
 # Create an instance of the imported SQLAlchemy class
 # Set to the instance of our Flask 'app'
